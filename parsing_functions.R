4eb0787782e10f2bb10d5b0540ed764a5d56eff7
Keita Abe <keita43a@gmail.com>
2023-03-06 18:00:17 +0900

    musashi update

--- parsing_functions.R
+++ parsing_functions.R
@@ -94,8 +94,8 @@ print_section <- function(position_data, section_id){
 
 # Construct a bar chart of skills
 build_skill_bars <- function(skills, out_of = 5){
-  bar_color <- "#365980"              #original "#969696"
-  bar_background <-"#baccde"          #original "#d9d9d9"
+  bar_color <- "#FFCD03"              #original "#969696"
+  bar_background <-"#FFF3C1"          #original "#d9d9d9"
   skills %>% 
     mutate(width_percent = round(100*level/out_of)) %>% 
     glue_data(
